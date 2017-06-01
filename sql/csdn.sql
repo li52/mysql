08f591929b47f6cbe6c2403ded7421aa8d74028b
li52 <572842612@qq.com>
2017-06-01 11:40:20 +0800

    utp

--- sql/csdn.sql
+++ sql/csdn.sql
@@ -8,6 +8,18 @@ CREATE TABLE db_1702.csdn(
 )
   COMMENT 'CSDN 用户表';
 
-SELECT *
+SELECT count(*)
 FROM db_1702.csdn;
+
+LOAD DATA LOCAL INFILE 'C:\\Users\\DELL\\Desktop\\www.csdn.net.sql'
+INTO TABLE db_1702.csdn
+FIELDS TERMINATED BY ' # ' (username, password, email)
+SET id = NULL;
+
+SELECT password
+FROM db_1702.csdn
+GROUP BY password
+ORDER BY count(password) DESC
+LIMIT 10;
+
 
